cdf5166488569ede76ffb87bf667ec366b340d57
Ben Fung Fook <benfungfook@gmail.com>
2015-11-01 20:30:00 -0500

    Create DataBase.cs

---
+++ DataBase.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class dataBase{
+    [XmlElement("AtomicNumber")]
+    public int atomicNo;
+}
